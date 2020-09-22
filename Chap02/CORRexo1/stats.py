89cb1d79908948178f33877db5fc9ba5012d2408
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-09-22 09:44:43 +0200

    Add files via upload

---
+++ Chap02/CORRexo1/stats.py
@@ -0,0 +1,9 @@
+﻿def total(t) :
+    somme = 0
+    for i in range(len(t)): # i parcourt les indices du tableau t
+        somme = somme + t[i]
+    return somme
+
+def moyenne(t):
+    return total(t)/len(t)
+
---
+++ Chap02/CORRexo1/test-stat.py
@@ -0,0 +1,5 @@
+﻿from stats import *
+
+assert total([1,2,3]) == 6
+assert moyenne([1,2,3]) == 2
+print('test ok')
