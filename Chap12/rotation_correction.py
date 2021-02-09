682199a364b55e596b9132867544c313e5a8f1b5
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-02-09 09:59:20 +0100

    Add files via upload

---
+++ Chap12/rotation_correction.py
@@ -0,0 +1,25 @@
+﻿def decoupe(px, x, y, t):
+    t = t//2
+    for i in range(t):
+        for j in range(t):
+            px[x+i ,y+j], px[x+t+i ,y+j], px[x+t+i ,y+t+j], px[x+i ,y+t+j]= \
+                   px[x+t+i ,y+j], px[x+t+i ,y+t+j], px[x+i ,y+t+j], px[x+i ,y+j]
+
+
+
+def rotation(px,x,y,t):
+    decoupe(px,x,y,t)
+    if t>1:
+        t = t//2
+        rotation(px,x,y,t)
+        rotation(px,x+t,y,t)
+        rotation(px,x,y+t,t)
+        rotation(px,x+t,y+t,t)
+
+
+
+from PIL import Image
+im = Image.open("tigre.jpg")
+px=im.load()
+rotation(px,0,0,256)
+im.save("rotation1.jpg")
