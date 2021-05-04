7bcb4d8ea662b37ccf366accc36d43c776cd60f2
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-05-04 09:45:29 +0200

    Update hotel.sql

--- Projets/Hotel/hotel.sql
+++ Projets/Hotel/hotel.sql
@@ -1,37 +1,36 @@
 CREATE TABLE IF NOT EXISTS Reservation (
-    id VARCHAR(50) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     prix VARCHAR(50),
-    arrivee VARCHAR(50),
-    depart VARCHAR(50),
-    idClient VARCHAR(50) REFERENCES Client(id)
+    arrivee DATE,
+    depart DATE,
+    idClient INT REFERENCES Client(id)
 );
 
 CREATE TABLE IF NOT EXISTS Client (
-    id VARCHAR(50) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(50),
     prenom VARCHAR(50),
     numero VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS Chambre (
-    id VARCHAR(50) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     numero VARCHAR(50),
-    places VARCHAR(50)
-);
-
-
-CREATE TABLE IF NOT EXISTS comporte (
-    idReservation VARCHAR(50) REFERENCES Reservation(id),
-    idChambre VARCHAR(50) REFERENCES Chambre(id),
-    PRIMARY KEY(idReservation,idChambre)
+    places INT
 );
 
 CREATE TABLE IF NOT EXISTS Breakfast (
-    id VARCHAR(50) PRIMARY KEY,
-    nombre VARCHAR(50)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre INT
 );
 
 CREATE TABLE IF NOT EXISTS Prix (
     prestation VARCHAR(50) PRIMARY KEY,
     tarif VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS comporte (
+    idReservation INT REFERENCES Reservation(id),
+    idChambre INT REFERENCES Chambre(id),
+    PRIMARY KEY(idReservation,idChambre)
 );
